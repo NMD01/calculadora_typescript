a40a91beed6eef7efd9dcec21b624d76cde05d83
Nathan Martini Duarte <nathan.martinid@gemail.com>
2025-04-15 18:22:04 -0300

    feito a parte logica com o typescript

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,23 @@
-console.log("calculadora!")
+const botoes = document.querySelectorAll<HTMLInputElement>(".botoes");
+const tela = document.querySelector<HTMLInputElement>("#tela")
+
+
+botoes.forEach((botao) => {
+    botao.addEventListener("click", () => {
+      if (!tela) return;
+  
+      if (botao.value === "=") {
+        try {
+          const resultado = eval(tela.value);
+          tela.value = resultado;
+        } catch (erro) {
+          tela.value = "Erro";
+        }
+      } else if(botao.value == "C") {
+        tela.value = ""
+      }else{
+        // Adiciona na tela apenas se NÃO for "="
+        tela.value += botao.value;
+      }
+    });
+  });
